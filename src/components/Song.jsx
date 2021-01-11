bddd552ded75682f9484635c7520d43b519e7902
cisu <49948070+cisu@users.noreply.github.com>
2021-01-11 21:54:10 +0200

    Fixed contol input and set currentTime: 0

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Song = ({currentSong}) => {
   return (
     <div className='song-container'>
-      <img src={currentSong.cover} alt='cover album' />
+      <img alt={currentSong.name} src={currentSong.cover} alt='cover album' />
       <h1>{currentSong.name}</h1>
       <h1>{currentSong.artist}</h1>
     </div>
