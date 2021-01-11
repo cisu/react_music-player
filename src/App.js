fa202e47bdbcff44cbde791232b42d5b4eb37993
cisu <49948070+cisu@users.noreply.github.com>
2021-01-11 20:50:17 +0200

    adding our state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+// Import Styles
+import './styles/app.scss';
+// Adding Components
+import Player from './components/Player';
+import Song from './components/Song';
+//Import data
+import chillhop from './data';
+//Util
+import {playAudio} from './util';
 
 function App() {
+  // State
+  const [songs, setSongs] = useState(chillhop());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+      <Song currentSong={currentSong} />
+      <Player />
     </div>
   );
 }
---
+++ src/components/Song.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Song = ({currentSong}) => {
+  return (
+    <div className='song-container'>
+      <img src={currentSong.cover} alt='cover album' />
+      <h1>{currentSong.name}</h1>
+      <h1>{currentSong.artist}</h1>
+    </div>
+  );
+};
+
+export default Song;
