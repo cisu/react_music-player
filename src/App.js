8c930f61d969dc74d4e10cda2edb08dac07f34fd
cisu <49948070+cisu@users.noreply.github.com>
2021-01-13 15:59:49 +0200

    Customizing input

--- src/App.js
+++ src/App.js
@@ -23,12 +23,26 @@ function App() {
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
     duration: 0,
+    animationPercentage: 0,
   });
 
   const timeUpdateHandler = e => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({...songInfo, currentTime: current, duration});
+    // Calculate Percentage
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animationPercentage = Math.round(
+      (roundedCurrent / roundedDuration) * 100
+    );
+    console.log(animationPercentage);
+
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage,
+    });
   };
   const [libraryStatus, setLibraryStatus] = useState(false);
 
